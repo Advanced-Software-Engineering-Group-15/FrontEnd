8557250225c817145aebec500e3b78d1fb858566
harryThompson141 <83436763+harryThompson141@users.noreply.github.com>
2022-02-03 16:40:35 +0000

    Adding Payment button

---
+++ components/stripe-button/stripe.button.component.jsx
@@ -0,0 +1,31 @@
+//    STRIPE PUBLISHABLE KEY available on Stripe Dashboard
+//    pk_test_51KP6MgG8vk1CJkKgWm5D962cWUXuxyFWpSJDDpFpDLjKmcAObx9kWrxrhX8DVc36ZwEBJdndG3EGX6bLHTsDd8vc00CvpXY7pQ 
+
+import React from 'react';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51KP6MgG8vk1CJkKgWm5D962cWUXuxyFWpSJDDpFpDLjKmcAObx9kWrxrhX8DVc36ZwEBJdndG3EGX6bLHTsDd8vc00CvpXY7pQ';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Succesful!');
+    };
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='Carma Corp'
+            billingAddress
+            shippingAddress
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
+
