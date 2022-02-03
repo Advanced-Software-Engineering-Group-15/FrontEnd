f91aa95a1b999106afb9759d59e0168099aafae6
Claudia Alonso <alonsovc@tcd.ie>
2022-02-03 17:16:51 +0000

    create_journey: Button works

--- pages/create_journey.tsx
+++ pages/create_journey.tsx
@@ -1,28 +1,21 @@
-import React from 'react';
-import { View, Text, Image, ScrollView, TextInput } from 'react-native';
-import SelectDropdown from 'react-native-select-dropdown'
-import NumberPlease from "react-native-number-please";
+import React, {useState} from 'react';
+import { View, Text, ScrollView, StyleSheet, TextInput, TouchableOpacity, Button } from 'react-native';
+import SelectDropdown from 'react-native-select-dropdown';
 
-const countries = ["Drive", "Cycle", "Walk"]
-//https://reactnativeexample.com/generate-react-native-pickers-with-range-numbers/
-const OrderPizza = () => {
-  const initialValues = [{ id: "pizza", value: 3 }];
-  const [pizzas, setPizzas] = useState(initialValues);
-  const pizzaNumbers = [{ id: "pizza", label: "?", min: 0, max: 99 }];
+const journeyType = ["Drive", "Cycle", "Walk"]
 
-  return (
-    <View>
-      <Text>I would like</Text>
-      <NumberPlease
-        digits={pizzaNumbers}
-        values={pizzas}
-        onChange={(values) => setPizzas(values)}
-      />
-    </View>
-  );
-}
+// const User = (props) => {
+// }
 
-const App = () => {
+const App = (props: any) => {
+  const [input, setInput] = useState({startJourney: ""});
+
+  const logValue = () => {
+    console.log(input);
+    //props.navigation.navigate("Home", { username: input })
+    props.navigation.navigate("Create_Journey", { username: input })
+  };
+
   return (
     <ScrollView>
       <View style={{
@@ -33,34 +26,79 @@ const App = () => {
         <Text>Select Journey Type:</Text>
         {/* https://reactnativeexample.com/a-highly-customized-dropdown-select-picker-menu-for-react-native/ */}
         <SelectDropdown
-          data={countries}
+          data={journeyType}
           onSelect={(selectedItem, index) => {
             console.log(selectedItem, index)
           }}
           buttonTextAfterSelection={(selectedItem, index) => {
-            // text represented after item is selected
-            // if data array is an array of objects then return selectedItem.property to render after item is selected
             return selectedItem
           }}
           rowTextForSelection={(item, index) => {
-            // text represented for each item in dropdown
-            // if data array is an array of objects then return item.property to represent item in dropdown
             return item
           }}
         />
+        <Text>Start of Journey:</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={text => setInput({startJourney: text})}/>
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text 
+          style={styles.loginText}
+          onPress={logValue}
+          >LOGIN</Text>
+        </TouchableOpacity>
+        <Button
+          onPress={logValue}
+          title="Learn More"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Text>End of Journey:</Text>
+        <TextInput
+        style={styles.input}
+        defaultValue=" "
+        />
         <Text>Cost of Journey:</Text>
-          
+        <TextInput
+        style={styles.input}
+        defaultValue=" "
+        />
       </View>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1
-        }}
-        defaultValue="You can type in me"
-      />
+      
     </ScrollView>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: 'gray',
+  },
+  loginBtn:{
+    width:"80%",
+    backgroundColor:"#fb5b5a",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+  loginText:{
+    color:"white"
+  }
+});
 
 export default App;
