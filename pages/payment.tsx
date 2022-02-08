41addb782f0179ef1bfa29c13ee5dcc1d07c58ef
harryThompson141 <83436763+harryThompson141@users.noreply.github.com>
2022-02-08 20:48:47 +0000

    Added Styling to File

--- pages/payment.tsx
+++ pages/payment.tsx
@@ -10,23 +10,66 @@ import StripeCheckout from 'react-stripe-checkout';
 
 import StripeCheckoutButton from '../components/stripe-button/stripe.button.component';
 
-function Payment() {
+const Payment = (props: any) => {
 
   const totalPrice = 100;
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Make Stripe Payment @ Carma</h1>
-        <p>
-          Pay Total of $ {totalPrice}
-        </p>
-        <p>
+    <View style={styles.container}>
+        <Text style={styles.logo}>Make Stripe Payment @ Carma</Text>
+        <Text style={styles.logo}>Pay Total of $ {totalPrice}</Text>
+        <TouchableOpacity style={styles.loginBtn}>
           <StripeCheckoutButton price={totalPrice} />
-        </p>
-      </header>
-    </div>
+        </TouchableOpacity>
+    </View>
   );
 }
+
+//styling
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#003f5c',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo:{
+    fontWeight:"bold",
+    fontSize:50,
+    color:"#fb5b5a",
+    marginBottom:40
+  },
+  inputView:{
+    width:"70%",
+    backgroundColor:"#465881",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  inputText:{
+    height:50,
+    color:"white"
+  },
+  forgot:{
+    color:"white",
+    fontSize:11
+  },
+  loginBtn:{
+    width:"35%",
+    backgroundColor:"#fb5b5a",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+  loginText:{
+    color:"white"
+  }
+});
 
 export default Payment;
